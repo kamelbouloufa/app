fed0430ba9e43b030677384026bb56ae7d89194b
kamelbouloufa <32460951+kamelbouloufa@users.noreply.github.com>
2025-02-20 00:17:52 +0100

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 app = Flask(__name__)
 
@@ -12,15 +14,20 @@ def visit():
         return jsonify({'error': 'يرجى إضافة رابط عبر ?url='}), 400
 
     options = Options()
-    options.add_argument("--headless")  # تشغيل بدون واجهة
+    options.add_argument("--headless")  
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--window-size=1920,1080")  # يحاكي شاشة كاملة
 
-    service = Service("E:/chromedriver-win64/chromedriver.exe")
+    service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
 
     try:
         driver.get(url)
+        time.sleep(5)  # انتظار تحميل الصفحة (يمكن تعديل المدة حسب الحاجة)
+
         return jsonify({'message': '✅ تم فتح الرابط بنجاح!', 'url': url})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
